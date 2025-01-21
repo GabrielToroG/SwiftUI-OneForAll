ec270aeecda0996b57fdb4090eea66be9f4eb0c2
Gabriel Toro <gtorowork@gmail.com>
2025-01-21 12:13:24 -0300

    Initial Commit

---
+++ SwiftUI-OneForAll/SwiftUI_OneForAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_OneForAllApp.swift
+//  SwiftUI-OneForAll
+//
+//  Created by Gabriel Alonso Toro Guzmán on 21-01-25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_OneForAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
