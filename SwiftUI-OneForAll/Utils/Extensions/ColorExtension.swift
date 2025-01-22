b71c636aeb30316c40b6be214c5bf83e94988db9
Gabriel Toro <gtorowork@gmail.com>
2025-01-22 02:21:23 -0300

    set colorExtension

---
+++ SwiftUI-OneForAll/Utils/Extensions/ColorExtension.swift
@@ -0,0 +1,31 @@
+//
+//  ColorExtension.swift
+//  SwiftUI-OneForAll
+//
+//  Created by Gabriel Alonso Toro Guzmán on 22-01-25.
+//
+
+import SwiftUI
+
+extension Color {
+    /// hex #E4D5C7 || brandColor
+    static let brandColor = Color("brandColor")
+    /// hex #CD9B51 || brownColor
+    static let brownColor = Color("brownColor")
+    /// hex #212224 || blackColor
+    static let blackColor = Color("blackColor")
+    /// hex #C5C6C7 || darkGreyColor
+    static let darkGreyColor = Color("darkGreyColor")
+    /// hex #F6F6F6 || grayColor
+    static let grayColor = Color("grayColor")
+    /// hex #FF8580 || redColor
+    static let redColor = Color("redColor")
+    /// hex #C8DBBE || secondaryColor
+    static let secondaryColor = Color("secondaryColor")
+    /// hex #212224 || transparentColor
+    static let transparentColor = Color("transparentColor")
+    /// hex #FFFFFF || whiteColor
+    static let whiteColor = Color("whiteColor")
+    /// hex #C5BA2C || yellowColor
+    static let yellowColor = Color("yellowColor")
+}
